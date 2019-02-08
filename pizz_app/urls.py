2bff62ec8ce84fef6a23bdc7676ce4628c26145c
Philipp26 <henrybauers@gmail.com>
2019-02-08 19:35:03 +0400

    Adding some templates

--- pizz_app/urls.py
+++ pizz_app/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
         #Home page
         url(r'^$', views.index, name='index'),
+        #Output all items (pizza)
+        url(r'^pizzas/$', views.pizzas, name='pizzas'),
+        #Page with available toppings for pizza
+        url(r'^pizzas/(?P<pizza_id>\d+)/$', views.pizza, name='pizza'),
         ]
--- pizz_app/views.py
+++ pizz_app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Pizza
 
 def index(request):
     return render(request, 'pizz_app/index.html')
+
+def pizzas(request):
+    "Output the list of pizzas"
+    pizzas = Pizza.objects.all()
+    context = {'pizzas' : pizzas}
+    return render(request, 'pizz_app/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    pizza = Pizza.objects.get(id = pizza_id)
+    toppings = pizza.topping_set.all()
+    context = {'pizza' : pizza, 'toppings' : toppings}
+    return render(request, "pizz_app/pizza.html", context)
